f329f641855dc721321e8da23a0a92a880c34800
Hector <hectorgetfree@gmail.com>
2025-04-02 21:52:32 +0800

    保存IDE配置

--- src/s_19/Solution_19.java
+++ src/s_19/Solution_19.java
@@ -1,5 +1,19 @@
 package s_19;
 
 public class Solution_19 {
-    
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if ()
+    }
 }
